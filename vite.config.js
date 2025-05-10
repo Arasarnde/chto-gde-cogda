b6102cd0e7252d2ad9389c5c2419a7944fc4071a
Sonya <sofia2525@yandex.ru>
2025-05-10 13:15:52 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/chto-gde-cogda/',
   plugins: [react()],
   assetsInclude: ['**/*.svg'],
   resolve: {
